6eff1f6596c562e9204fd654a08ec3ead270fd88
Ryan Clark <Rymaster95@gmail.com>
2017-03-15 16:57:09 -0600

    finished first 3 videos of redux introduction

---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className="single-photo">
+        Im the single grid
+      </div>
+    )
+  }
+})
+export default Single;
