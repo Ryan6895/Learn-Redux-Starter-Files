a31c0574a22b563dba92d6ec159bc9f582e19873
Ryan Clark <Rymaster95@gmail.com>
2017-04-17 12:03:12 -0400

    added single component and comments component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Photo from './photo';
+import Comments from './comments';
+
 
 const Single = React.createClass({
   render() {
+
+const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+console.log(i);
+const post = this.props.posts[i];
+console.log(post);
+
     return (
       <div className="single-photo">
-        Im the single grid
+        <Photo i={i} post={post} {...this.props}/>
+        <Comments></Comments>
       </div>
     )
   }
